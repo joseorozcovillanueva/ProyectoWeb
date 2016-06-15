0488c6b9a5c557f8949ba2ea0f477366680431ab
José Orozco Villanueva <orozco.jose@hotmail.com>
2016-06-15 12:44:23 -0500

    creación de la clase  en entidad llamada Auditoria

---
+++ ExpedienteElectronico/Entidades/Auditoria.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Auditoria
+    {
+        /// <summary>
+        /// Creación de la entidad de auditoria
+        /// </summary>
+        public int idAuditoria { get; set; }
+        public string cNombreMaquina { get; set; }
+        public DateTime dtFechaOperacion { get; set; }
+        public int idUsuario { get; set; }
+        public string cIPMaquina { get; set; }
+        public string cOperacion { get; set; }
+        public string cValor { get; set; }
+
+        /// <summary>
+        /// Constructor default
+        /// </summary>
+        public Auditoria() { }
+
+    }
+}
